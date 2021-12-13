381aca0ae26a8972ab6b0849b3fa2ddf17269bd6
HeitorDJAk47Gamer <73564945+HeitorDJAk47Gamer@users.noreply.github.com>
2021-12-13 13:08:01 -0300

    logs update

---
+++ cogs/logs.py
@@ -0,0 +1,22 @@
+import discord
+import os
+from discord.ext import commands
+import datetime
+
+class logs_cog(commands.Cog):
+	def __init__ (self, lara):
+		self.lara = lara
+
+	@commands.Cog.listener()
+	async def on_command(self, ctx):
+		server = ctx.guild.name
+		user = ctx.author
+		comando = ctx.command
+		timestamp = datetime.datetime.utcnow().strftime(f'Data: %d/%m/%Y Hora: %H:%M:%S %p')
+
+		with open('logs.txt', 'a') as ls:
+			ls.write(f'{server} > {user} > {comando} > {timestamp}\n')
+		ls.close()
+
+def setup(lara):
+	lara.add_cog(logs_cog(lara))
