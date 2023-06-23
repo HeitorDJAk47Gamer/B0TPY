74db36a510732f30c21b6111b490d8d042a7c3e8
Heitor Machado <73564945+HeitorDJAk47Gamer@users.noreply.github.com>
2023-06-22 23:24:51 -0300

    Atualizar o music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from youtube_dl import YoutubeDL
+from youtube_dl import YoutubeDL #codigo descontinuado!
 
 class music(commands.Cog):
     def __init__(self, lara):
